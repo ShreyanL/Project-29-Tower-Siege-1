f288f30e9ac418184a68b3565eac8dcf56233e5a
ShreyanL <shreyan.lowalekar@gmail.com>
2020-12-20 14:30:27 +0530

    first commit

---
+++ Box.js
@@ -0,0 +1,9 @@
+class Box extends BaseClass 
+{
+  constructor(x, y, width, height, fill_color)
+  {
+    super (x, y, width, height, fill_color, 0);
+    //this.image = loadImage("sprites/wood1.png");
+  }
+
+};
